baece06dab744d3fe157e72b6d0e69774f101505
Kuba Birecki <kuba.birecki@crystalplanet.io>
2019-12-07 15:22:28 +0100

    Add solution for day 5

--- src/IntComputer.elm
+++ src/IntComputer.elm
@@ -1,34 +1,49 @@
-module IntComputer exposing (intComputer, getInt, findIntComputerInput)
+module IntComputer exposing (..)
 
-import Array exposing (Array, fromList, toList, get, set, slice)
-
-type alias Params = (Int, Int, Int)
+import Array exposing (Array, get, set)
+import List exposing (append)
+import Tuple exposing (first)
 
 getInt: Int -> Array Int -> Int
 getInt index array =
     Maybe.withDefault 0 (get index array)
 
-apply: (Int -> Int -> Int) -> Params -> Array Int -> Array Int
-apply func (a, b, saveTo) memory =
-    set saveTo (func (getInt a memory) (getInt b memory)) memory
+readArg: Int -> Int -> Array Int -> Int
+readArg index mode state =
+    case mode of
+        0 -> readArg (getInt index state) 1 state
+        1 -> getInt index state
+        _ -> 0
 
-intComputer: Int -> Array Int -> Array Int
-intComputer position memory =
+intComputer: Int -> List Int -> List Int -> Array Int -> (Array Int, List Int)
+intComputer position input output state =
     let
-        next =
-            position + 4
-        operation =
-            toList (slice position next memory)
+        opcode =
+            (getInt position state)
+        instruction =
+            modBy 100 opcode
+        mode =
+            (\i -> modBy 10 (opcode // 10 ^ (i + 1)))
+        write =
+            (\i v -> (set (getInt (position + i) state) v) state)
+        read =
+            (\i -> readArg (position + i) (mode i) state)
     in
-    case operation of
-        1::i::j::saveTo::_ -> intComputer next (apply (+) (i, j, saveTo) memory)
-        2::i::j::saveTo::_ -> intComputer next (apply (*) (i, j, saveTo) memory)
-        99::_ -> memory
-        _ -> fromList []
+    case (instruction, input) of
+        (1, _) -> intComputer (position + 4) input output (write 3 ((read 1) + (read 2)))
+        (2, _) -> intComputer (position + 4) input output (write 3 ((read 1) * (read 2)))
+        (3, value::rest) -> intComputer (position + 2) rest output (write 1 value)
+        (4, _) -> intComputer (position + 2) input (append output [(read 1)]) state
+        (5, _) -> intComputer (if (read 1) == 0 then position + 3 else (read 2)) input output state
+        (6, _) -> intComputer (if (read 1) /= 0 then position + 3 else (read 2)) input output state
+        (7, _) -> intComputer (position + 4) input output (write 3 (if (read 1) < (read 2) then 1 else 0))
+        (8, _) -> intComputer (position + 4) input output (write 3 (if (read 1) == (read 2) then 1 else 0))
+        (99, _) -> (state, output)
+        _ -> (state, output)
 
 findIntComputerInput: Int -> Int -> Int -> Array Int -> (Int, Int)
 findIntComputerInput x y expectedResult initialState =
-    let output = initialState |> set 1 x |> set 2 y |> (intComputer 0) |> getInt 0
+    let output = initialState |> set 1 x |> set 2 y |> (intComputer 0 [] []) |> first |> getInt 0
     in
     case (output == expectedResult, x, y) of
         (True, _, _) -> (x, y)
