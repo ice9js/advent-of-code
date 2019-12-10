409f9e80feeeb73bca5dc533a29eadc92eea4c4a
Kuba Birecki <kuba.birecki@crystalplanet.io>
2019-12-10 22:28:24 +0100

    Add solution for part two of day 10

--- src/AsteroidMap.elm
+++ src/AsteroidMap.elm
@@ -1,11 +1,10 @@
 module AsteroidMap exposing (..)
 
 import Array exposing (Array, get)
-import Dict exposing (Dict, keys, update)
-import List exposing (filterMap, foldl, foldr, head, isEmpty, length, map, sort, tail)
-import Set exposing (Set)
+import Dict exposing (Dict, update)
+import List exposing (drop, filterMap, head, isEmpty, length, map, reverse, sort, sortBy)
 import String exposing (split, trim)
-import Tuple exposing (mapBoth, first, second)
+import Tuple exposing (mapBoth)
 
 type alias Polar = (Float, Float)
 
@@ -39,22 +38,30 @@ fromString src =
 
 getRelativePosition: Asteroid -> Asteroid -> Maybe Polar
 getRelativePosition (x, y) asteroid =
-    let substractFrom = (\n -> ((-) n) >> toFloat)
+    let substract = (\a b -> toFloat (b - a))
     in
     case (x, y) == asteroid of
         True -> Nothing
-        _ -> Just (toPolar (mapBoth (substractFrom x) (substractFrom y) asteroid))
+        _ -> Just (toPolar (mapBoth (substract x) (substract y) asteroid))
 
-getAbsolutePosition: Asteroid -> Polar -> Asteroid
+getAbsolutePosition: Asteroid -> Maybe Polar -> Maybe Asteroid
 getAbsolutePosition (x, y) coordinates =
-    let addTo = (\n -> round >> ((+) n))
+    let addTo = (\a b -> a + (round b))
     in
-    mapBoth (addTo x) (addTo y) (fromPolar coordinates)
+    case coordinates of
+        Just c -> Just (mapBoth (addTo x) (addTo y) (fromPolar c))
+        Nothing -> Nothing
+
+turnsToVertical: Float -> Float
+turnsToVertical angle =
+    case angle < -0.5 * pi of
+        True -> 2 * pi + angle
+        _ -> angle
 
 buildMap: List Polar -> AsteroidMap
 buildMap coordinates =
     case coordinates of
-        (distance, angle)::rest -> update angle (\v -> Just ([distance] ++ (Maybe.withDefault [] v))) (buildMap rest)
+        (distance, angle)::rest -> update (turnsToVertical angle) (\v -> Just ([distance] ++ (Maybe.withDefault [] v))) (buildMap rest)
         [] -> Dict.empty
 
 initMap: List Asteroid -> Asteroid -> AsteroidMap
@@ -63,9 +70,52 @@ initMap asteroids origin =
         |> buildMap
         |> Dict.map (\_ distances -> sort distances)
 
+getVisible: AsteroidMap -> List Polar
+getVisible asteroidMap =
+    Dict.map (\angle distances -> (Maybe.withDefault 0 (head distances), angle)) asteroidMap
+        |> Dict.values
+
+getVisibleNumber: AsteroidMap -> Int
+getVisibleNumber asteroidMap =
+    length (getVisible asteroidMap)
+
+dropVisible: AsteroidMap -> AsteroidMap
+dropVisible asteroidMap =
+    Dict.map (\_ l -> drop 1 l) asteroidMap
+        |> Dict.filter (\_ l -> not (isEmpty l))
+
+getFiringOrder: AsteroidMap -> List Polar
+getFiringOrder asteroidMap =
+    let
+        visible =
+            getVisible asteroidMap
+        remaining =
+            dropVisible asteroidMap
+    in
+    case Dict.isEmpty remaining of
+        True -> visible
+        _ -> visible ++ (getFiringOrder remaining)
+
+findAsteroidByMostVisibleAsteroids: List Asteroid -> Maybe Asteroid
+findAsteroidByMostVisibleAsteroids asteroids =
+    asteroids
+        |> sortBy ((initMap asteroids) >> getVisibleNumber)
+        |> reverse
+        |> head
+
+findAsteroidByFiringOrder: Int -> List Asteroid -> Maybe Asteroid
+findAsteroidByFiringOrder order asteroids =
+    case findAsteroidByMostVisibleAsteroids asteroids of
+        Just station ->
+            initMap asteroids station
+                |> getFiringOrder
+                |> Array.fromList
+                |> get (order - 1)
+                |> getAbsolutePosition station
+        Nothing -> Nothing
+
 findMostVisibleAsteroids: List Asteroid -> Int
 findMostVisibleAsteroids asteroids =
-    let numberOfVisibleAsteroids = (initMap asteroids) >> keys >> length
-    in
-    map numberOfVisibleAsteroids asteroids
-        |> foldl max 0
+    case findAsteroidByMostVisibleAsteroids asteroids of
+        Just a -> getVisibleNumber (initMap asteroids a)
+        Nothing -> 0
